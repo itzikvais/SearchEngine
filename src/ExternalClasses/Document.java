4fd27fdff02269deba439d6e522ffcd6ac699055
tsizer <tsizer@post.bgu.ac.il>
2018-11-30 18:38:51 +0200

    Document Class update- add path, startLine and endLine fields.

--- src/ExternalClasses/Document.java
+++ src/ExternalClasses/Document.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class Document {
     public static HashMap<String,Document> docCollection = new HashMap<>(); //all the docs in this chunk
     private String docID;
+    private String filePath;
+    private int startLine;
+    private int endLine;
     public HashMap<Term, Integer> docTermsAndCount;
     private int uniqueTermsCounter;
     private String mostFreqTerm;
@@ -12,11 +15,26 @@ public class Document {
     private String cityOfOrigin;
     private int docLength;
 
-    public Document(String docID, String cityOfOrigin) {
+    public Document(String docID, String cityOfOrigin, String filePath, int startLine, int endLine) {
         this.docID = docID;
         this.docTermsAndCount = new HashMap<>();
         this.cityOfOrigin = cityOfOrigin;
+        this.filePath = filePath;
+        this.startLine = startLine;
+        this.endLine = endLine;
         docCollection.put(docID,this);
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public int getStartLine() {
+        return startLine;
+    }
+
+    public int getEndLine() {
+        return endLine;
     }
 
     @Override
@@ -26,7 +44,7 @@ public class Document {
         return docID;
     }
 
-    public boolean isUniqueTerm(Term term){ return !docTermsAndCount.containsKey(term);}
+    private boolean isUniqueTerm(Term term){ return !docTermsAndCount.containsKey(term);}
 
     public void addTerm(Term term){
         if(isUniqueTerm(term)){
